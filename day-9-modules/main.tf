21bfb2ad58cb4c746a336ef910891d7ae2730637
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-03-23 18:00:53 +0530

    Create main.tf

---
+++ day-9-modules/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "dev" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    associate_public_ip_address = true
+    tags = {
+      Name = var.tags
+    }
+}
