18bf4c78bb654fd44558c8e083b3494ba29db7eb
CloudTechDevOps <cloud87777@gmail.com>
2024-03-23 18:39:46 +0530

    updates

--- day-9-modules/child.tf
+++ day-9-modules/child.tf
@@ -1,5 +1,5 @@
 module "test" {
-    source = "./modules/module" 
+    source = "./module" 
     ami       	= "ami-0440d3b780d96b29d"
     instance_type = "t2.micro"
     subnet_id 	= "subnet-0f2a81412b51a8ae0"
