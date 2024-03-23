c3a721501ec29cdd7e0dda33c6427eb5f76bec50
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-03-23 18:01:17 +0530

    Create variables.tf

---
+++ day-9-modules/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+    description = "passing AMI vlaue to  main.tf"
+    type = string
+    default = ""
+  
+}
+variable "instance_type" {
+  type = string
+  default = ""
+}
+variable "key_name" {
+    type = string
+    default = ""
+  
+}
+variable "tags" {
+    default = ""
+  
+}
