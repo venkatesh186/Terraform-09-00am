9e6ded9769c1f8beea67542bcdba23f78100a7f3
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-03-23 18:02:56 +0530

    Delete day-9-modules directory

--- day-9-modules/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "dev" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-    associate_public_ip_address = true
-    tags = {
-      Name = var.tags
-    }
-}
