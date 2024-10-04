8ecf4b0e86d83035479270d742c1893d9027f2db
venkatesh186 <arikicherlavenkatesulu@gmail.com>
2024-10-04 16:39:37 +0530

    Update main.tf

--- day-2-basic_code_ec2/main.tf
+++ day-2-basic_code_ec2/main.tf
@@ -2,12 +2,8 @@ resource "aws_instance" "test" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    associate_public_ip_address = true
     tags = {
       Name = "dev_ec2"
     }
 }
 
-provider "aws" {
-   region = "ap-south-1"
-}
