7a3e869a1b2b8c8eb779bb2ecae49a3a588c5561
venkatesh186 <arikicherlavenkatesulu@gmail.com>
2024-11-08 16:30:01 +0530

    Update main.tf

--- day-2-basic_code_ec2/main.tf
+++ day-2-basic_code_ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
-    ami = "var.ami_id"
-    instance_type = "var.instance_type"
-    key_name = "var.key_name"
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
     tags = {
       Name = "dev_ec2"
     }
