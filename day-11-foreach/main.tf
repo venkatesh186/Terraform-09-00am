00d7dd4359e2973d32329ec2e27407e7e5bc0c2a
venkatesh186 <arikicherlavenkatesulu@gmail.com>
2024-11-13 16:22:25 +0530

    Update main.tf

--- day-11-foreach/main.tf
+++ day-11-foreach/main.tf
@@ -7,6 +7,10 @@ variable "instance_type" {
   type    = string
   default = "t2.micro"
 }
+variable "key_name" {
+type      = string
+default   = "ram"
+}
 
 variable "sandboxes" {
   type    = list(string)
